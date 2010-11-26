810fe6e16c4dc0321b98cb4be21466c372e68c5f
Manuel Giraud <manuel.giraud@univ-nantes.fr>
2010-11-26 13:23:11 +0100

    emacs
    Add emacs launcher.
    Make wm.lisp an sbcl script.

--- wm.lisp
+++ wm.lisp
@@ -1,21 +1,32 @@
+#!/usr/local/bin/sbcl --script
+;;; Load CLX stuff
 (require 'asdf)
 (asdf:oos 'asdf:load-op 'clx)
 (use-package :xlib)
-(defparameter *mods*    '(:mod-1))      ; :mod-4 for windows key
-(defparameter *move*    '(1))
-(defparameter *resize*  '(2))
-(defparameter *lower*   '(3 4))
-(defparameter *raise*   '(5))
+
+;;; Most simple window manager on earth (fork from the lisp version of
+;;; tinywm).
+(defparameter *mods* '(:mod-1))      ; :mod-4 for windows key
+(defparameter *move* '(1))
+(defparameter *resize* '(2))
+(defparameter *lower* '(3 4))
+(defparameter *raise* '(5))
+(defparameter *f12* #xffc9
+  "F12 keysym.")
 
 (defun main ()
   (let* ((display (open-display ""))
          (screen (first (display-roots display)))
          (root (screen-root screen)))
 
+    ;; Grab mouse buttons
     (dolist (button (list *move* *resize* *lower* *raise*))
       (mapcar #'(lambda (b)
                   (grab-button root b '(:button-press) :modifiers *mods*))
               button))
+
+    ;; Grab F12 to launch emacs back
+    (grab-key root (keysym->keycodes display *f12*))
 
     (unwind-protect
         (let (last-button last-x last-y)
@@ -24,6 +35,10 @@
              (display :discard-p t)
              ;; for key-press and key-release, code is the keycode
              ;; for button-press and button-release, code is the button number
+             (:key-press
+              ()
+              ;; Not really portable isn't it
+              (sb-ext:run-program "emacs" nil :wait nil :search t))
              (:button-press 
               (code child)
               (cond ((member code *raise*)
@@ -40,7 +55,6 @@
                                last-y (seventh lst)))))))
              (:motion-notify
               (event-window root-x root-y)
-              ;; while(XCheckTypedEvent(display, MotionNotify, &ev));
               (cond ((member last-button *move*)
                      (let ((delta-x (- root-x last-x))
                            (delta-y (- root-y last-y)))
@@ -53,10 +67,6 @@
                            (new-h (max 1 (- root-y (drawable-y event-window)))))
                        (setf (drawable-width event-window) new-w
                              (drawable-height event-window) new-h)))))
-                       ;; (setf (drawable-width event-window)
-                       ;;       (max 1 (- root-x (drawable-x event-window)))
-                       ;;       (drawable-height event-window)
-                       ;;       (max 1 (- root-y (drawable-y event-window))))))))
              (:button-release ()
                               (ungrab-pointer display)))))
       (dolist (button (list *move* *resize* *lower* *raise*))
