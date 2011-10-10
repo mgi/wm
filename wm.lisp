910ed16994a01b5c054713d6168531ae08f7793b
Manuel Giraud <manuel.giraud@univ-nantes.fr>
2011-10-10 17:03:42 +0200

    remove useless intermediate variables

--- wm.lisp
+++ wm.lisp
@@ -136,12 +136,13 @@ and don't add window already in the list."
 (defror xxxterm)
 
 ;;; Mouse shorcuts
-(defparameter *move* '(:mod-1 1) "Mouse button to move a window")
-(defparameter *resize* '(:mod-1 3) "Mouse button to resize a window")
-(defparameter *kill* '(:control :mod-1 2) "Mouse button to kill a window")
+(defparameter *move* (compile-shortcut '(:mod-1 1)) "Mouse button to move a window")
+(defparameter *resize* (compile-shortcut '(:mod-1 3)) "Mouse button to resize a window")
+(defparameter *kill* (compile-shortcut '(:control :mod-1 2))
+  "Mouse button to kill a window")
 
 ;;; Key shortcuts
-(defparameter *prefix* '(:control #\t) "Prefix for shortcuts")
+(defparameter *prefix* (compile-shortcut '(:control #\t)) "Prefix for shortcuts")
 (defshortcut (#\c) (run-program "xterm" nil :wait nil :search t))
 (defshortcut (#\e) (emacs))
 (defshortcut (#\w) (xxxterm))
@@ -162,17 +163,13 @@ and don't add window already in the list."
 
 ;;; Main
 (defun main ()
-  (let ((prefix (compile-shortcut *prefix*))
-        (move (compile-shortcut *move*))
-        (resize (compile-shortcut *resize*))
-        (kill  (compile-shortcut *kill*))
-        last-button last-x last-y waiting-shortcut)
+  (let (last-button last-x last-y waiting-shortcut)
 
     ;; Grab prefix and mouse buttons on root
-    (grab-key *root* (code prefix) :modifiers (state prefix))
-    (grab-button *root* (code move) '(:button-press) :modifiers (state move))
-    (grab-button *root* (code resize) '(:button-press) :modifiers (state resize))
-    (grab-button *root* (code kill) '(:button-press) :modifiers (state kill))
+    (grab-key *root* (code *prefix*) :modifiers (state *prefix*))
+    (grab-button *root* (code *move*) '(:button-press) :modifiers (state *move*))
+    (grab-button *root* (code *resize*) '(:button-press) :modifiers (state *resize*))
+    (grab-button *root* (code *kill*) '(:button-press) :modifiers (state *kill*))
 
     ;; Populate list of windows
     (loop for w in (query-tree *root*) do
@@ -197,20 +194,20 @@ and don't add window already in the list."
                                      ((eq fn 'quit) (loop-finish))))))
                          (ungrab-keyboard *display*)
                          (setf waiting-shortcut nil)))
-                      ((and (= state (state prefix)) (= code (code prefix)))
+                      ((and (= state (state *prefix*)) (= code (code *prefix*)))
                        (grab-keyboard *root*)
                        (setf waiting-shortcut t))))
                (:button-press
                 (code state child)
                 (when (and child (eql (window-override-redirect child) :off))
-                  (cond ((and (= code (code kill))
-                              (= state (state kill)))
+                  (cond ((and (= code (code *kill*))
+                              (= state (state *kill*)))
                          (kill-client *display* (window-id child)))
                         (t
                          (setf last-button code)
                          (grab-pointer child '(:pointer-motion :button-release))
-                         (when (and (= code (code resize))
-                                    (= state (state resize)))
+                         (when (and (= code (code *resize*))
+                                    (= state (state *resize*)))
                            (warp-pointer child (drawable-width child)
                                          (drawable-height child)))
                          (let ((lst (multiple-value-list (query-pointer *root*))))
@@ -218,14 +215,14 @@ and don't add window already in the list."
                                  last-y (seventh lst)))))))
                (:motion-notify
                 (event-window root-x root-y)
-                (cond ((= last-button (cdr move))
+                (cond ((= last-button (code *move*))
                        (let ((delta-x (- root-x last-x))
                              (delta-y (- root-y last-y)))
                          (incf (drawable-x event-window) delta-x)
                          (incf (drawable-y event-window) delta-y)
                          (incf last-x delta-x)
                          (incf last-y delta-y)))
-                      ((= last-button (cdr resize))
+                      ((= last-button (code *resize*))
                        (let ((new-w (max 1 (- root-x (drawable-x event-window))))
                              (new-h (max 1 (- root-y (drawable-y event-window)))))
                          (setf (drawable-width event-window) new-w
@@ -242,10 +239,10 @@ and don't add window already in the list."
                   (when (win= window *last*)
                     (setf *last* (when (listp *last*)
                                    (rrem window *last* :test #'window-equal))))))))
-      (ungrab-button *root* (code move) :modifiers (state move))
-      (ungrab-button *root* (code resize) :modifiers (state resize))
-      (ungrab-button *root* (code kill) :modifiers (state kill))
-      (ungrab-key *root* (code prefix) :modifiers (state prefix))
+      (ungrab-button *root* (code *move*) :modifiers (state *move*))
+      (ungrab-button *root* (code *resize*) :modifiers (state *resize*))
+      (ungrab-button *root* (code *kill*) :modifiers (state *kill*))
+      (ungrab-key *root* (code *prefix*) :modifiers (state *prefix*))
       (close-display *display*))))
 
 (main)
