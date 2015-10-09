b939c42ee69b1ccea2c2876ce8d08af1390458a5
Manuel Giraud <manuel@ledu-giraud.fr>
2015-10-09 16:16:12 +0200

    more pretty printed keys

--- wm.lisp
+++ wm.lisp
@@ -71,7 +71,12 @@
   (let* ((state (make-state-keys (first (states sc))))
          (code (code sc))
          (k (if (< code 4) code (keycode->character *display* code 0))))
-    (format stream "(~{~s ~}~s)" state k)))
+    (when (member :shift state)
+      (setf state (remove :shift state)
+            k (char-upcase k)))
+    (format stream "~a~a" (concatenate 'string
+                                       (when (member :control state) "C-")
+                                       (when (member :mod-1 state) "M-")) k)))
 
 (defun compile-shortcut (&rest l)
   "Compile a shortcut. Usage: (compile-shortcut :control #\t) for a
