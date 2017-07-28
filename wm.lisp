eced8602206ee841138b75804a5c787cf2e78bdd
Manuel Giraud <manuel@ledu-giraud.fr>
2017-07-28 17:08:48 +0200

    more complex close mouse button

--- wm.lisp
+++ wm.lisp
@@ -400,7 +400,7 @@ don't contain `sofar'."
 (defparameter *quit* (compile-shortcut :shift #\q) "Shortcut to quit")
 (defparameter *move* (compile-shortcut :mod-1 1) "Mouse button to move a window")
 (defparameter *resize* (compile-shortcut :mod-1 3) "Mouse button to resize a window")
-(defparameter *close* (compile-shortcut :control :mod-1 2) "Mouse button to close a window")
+(defparameter *close* (compile-shortcut :mod-1 :control :shift 2) "Mouse button to close a window")
 
 (defun send-prefix (window)
   (xlib:send-event window :key-press (xlib:make-event-mask :key-press)
