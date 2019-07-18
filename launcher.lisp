cb0b5915d603e3ae1773b26b21e20f33f12320c9
Manuel Giraud <manuel@ledu-giraud.fr>
2019-07-18 17:44:35 +0200

    no required dependency on quicklisp.

---
+++ launcher.lisp
@@ -0,0 +1,3 @@
+(require :asdf)
+(asdf:load-system :wm)
+;; not reached
--- ql-launcher.lisp
+++
@@ -1,4 +0,0 @@
-(load (merge-pathnames "prog/lisp/quicklisp/setup.lisp" (user-homedir-pathname)))
-(push (directory-namestring *load-pathname*) asdf:*central-registry*)
-(ql:quickload 'wm)
-;; not reached
